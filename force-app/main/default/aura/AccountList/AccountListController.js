0a8b1fad2808b1648b43263f47e2ebc265fa56d4
Mike Miller <prg114@BCC.local>
2018-12-11 11:01:16 -0600

    added map markers

--- force-app/main/default/aura/AccountList/AccountListController.js
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -3,6 +3,10 @@
         var action = component.get("c.findAll");
         action.setCallback(this, function (a) {
             component.set("v.accounts", a.getReturnValue());
+            var event = $A.get("e.c:AccountsLoaded");
+            event.setParams({"accounts": a.getReturnValue()});
+            console.log('fire event');
+            event.fire();
         });
         $A.enqueueAction(action);
     }
